14d7515488012ea694ad06e9a9588d9743e626d7
13714093081@163.com <13714093081@163.com>
2019-03-04 11:18:33 +0800

    Commit end

---
+++ netty-api/src/main/java/com/netty/demo/api/DefaultNettyInfo.java
@@ -0,0 +1,10 @@
+package com.netty.demo.api;
+
+/**
+ * @Auther: wangjian
+ * @Date: 2018-10-11 10:41:03
+ */
+public class DefaultNettyInfo {
+
+    public static final  String  SPLIT_SMBOL = "%S%" ; // 分隔符
+}
